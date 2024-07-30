9fd5a5748ffe97d6226a5a325f27bffa2b9e2585
anishsaha46 <anish.ece321046@bppimt.ac.in>
2024-07-30 20:08:51 +0530

    Initial commit of Qr Code genrator

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Qr from './Qr';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1 className="app-heading">QR Code Generator</h1>
+      <Qr />
+    </div>
+  );
+}
+
+export default App;
+
